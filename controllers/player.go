3e3b91254b3c38d5faa5b247b989f17a0a8eb782
Lingbo-Huang <424559864@qq.com>
2024-08-19 22:06:23 +0800

    mysql用Order实现排行

--- controllers/player.go
+++ controllers/player.go
@@ -12,10 +12,21 @@ type PlayerController struct{}
 func (p PlayerController) GetPlayers(c *gin.Context) {
 	aidStr := c.DefaultPostForm("aid", "0")
 	aid, _ := strconv.Atoi(aidStr)
-	rs, err := models.GetPlayers(aid)
+	rs, err := models.GetPlayers(aid, "id asc")
 	if err != nil {
 		ReturnError(c, 4004, fmt.Sprintf("没有%d号活动的参赛选手相关信息", aid))
 		return
 	}
 	ReturnSuccess(c, 0, "success", rs, 1)
+}
+
+func (p PlayerController) GetRanking(c *gin.Context) {
+	aidStr := c.DefaultPostForm("aid", "0")
+	aid, _ := strconv.Atoi(aidStr)
+	rs, err := models.GetPlayers(aid, "score desc")
+	if err != nil {
+		ReturnError(c, 4004, fmt.Sprintf("没有%d号活动的参赛选手相关信息", aid))
+	} else {
+		ReturnSuccess(c, 0, "success", rs, 1)
+	}
 }
